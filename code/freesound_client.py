5bf42778af6f02076f4bb665905c5987bd0fbf97
Ferran Mesas <ferran.mesas01@gmail.com>
2014-06-12 14:41:44 +0200

    added embed URL for freesound sounds.

--- code/freesound_client.py
+++ code/freesound_client.py
@@ -11,4 +11,9 @@ def get_sound(tags, coord = None, radius = None):
     else:
         query_result = client.text_search(query = tags, page_size = 25)
 
-    return [freesound.Sound(s, client) for s in query_result.results]
+    result = []
+    for s in query.results:
+        sound = freesound.Sound(s, client)
+        sound.embed_url = "http://www.freesound.org/embed/sound/iframe/" + sound.id + "/simple/medium/"
+        result.append(sound)
+    return result
