aa5e6beae62285704cdbc7a74427265164e47138
Ferran Mesas <ferran.mesas01@gmail.com>
2014-06-12 18:36:21 +0200

    Added descriptions for freesound sounds.

--- code/freesound_client.py
+++ code/freesound_client.py
@@ -11,9 +11,8 @@ def get_sound(tags, coord = None, radius = None):
     else:
         query_result = client.text_search(query = tags, page_size = 25)
 
-    result = []
-    for s in query_result.results:
-        sound = freesound.Sound(s, client)
-        sound.embed_url = "http://www.freesound.org/embed/sound/iframe/" + str(sound.id) + "/simple/medium/"
-        result.append(sound)
-    return result
+    for sound in query_result.results:
+        sound["embed_url"] = "http://www.freesound.org/embed/sound/iframe/" + str(sound["id"]) + "/simple/medium/"
+        sound["description"] = client.get_sound(sound["id"]).description
+
+    return query_result.results
