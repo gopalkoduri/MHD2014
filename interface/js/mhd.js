40a5bd60c91a91ce1fa4771ae72caabde7b4f621
Ferran Mesas <ferran.mesas01@gmail.com>
2014-06-13 09:00:18 +0200

    added a fake loading bar

--- interface/js/mhd.js
+++ interface/js/mhd.js
@@ -1,34 +1,30 @@
-var current_elements = {images:[], sounds:[]}
-make_embed_sound = function(sound) {
-    return "<div class=\"sound\" " + "id=\"" + sound.id + "\">" +
-        "<img src=\"/interface/images/thumbsup-small.png\" class=\"click\" onclick=\"click_element(" +sound.id + ")\" />" +
-           "<iframe class=\"content\" frameborder=\"0\" scrolling=\"no\" src=\"" +
-           sound.embed_url + "\" width=\"481\" height=\"86\"></iframe></div>";
-};
+var current_elements = {images: [], sounds: []};
+var make_embed_sound = function (sound) {
+        "use strict";
+        return "<div class=\"sound\" " + "id=\"" + sound.id + "\">" +
+            "<img src=\"/interface/images/thumbsup-small.png\" class=\"click\" onclick=\"click_element('s'," + sound.id + ")\" />" +
+            "<iframe class=\"content\" frameborder=\"0\" scrolling=\"no\" src=\"" +
+            sound.embed_url + "\" width=\"481\" height=\"86\"></iframe></div>";
+    };
 
-make_embed_image = function(image) {
-    return "<div class=\"image\" " + " id=\"" +image.id + "\">" +
-        "<img src=\"/interface/images/thumbsup-small.png\" class=\"click\" onclick=\"click_element(" + image.id + ")\" />" +
-        "<img class=\"content\" src=\"" + image.embed_url+ "\" /></div>";
-}
+var make_embed_image = function (image) {
+        "use strict";
+        return "<div class=\"image\" " + " id=\"" + image.id + "\">" +
+            "<img src=\"/interface/images/thumbsup-small.png\" class=\"click\" onclick=\"click_element('p'," + image.id + ")\" />" +
+            "<img class=\"content\" src=\"" + image.embed_url + "\" /></div>";
+    };
 
-click_element = function(target) {
-    console.log(target);
-    var current_ids = {images: [], sounds: []}
+var click_element = function(type, target) {
+    $("#progressbar").progressbar("option", "value", 0);
+    console.log("getting more things like  " +  target);
+    var current_ids = {images: [], sounds: []};
 
-    var type;
     $.each(current_elements.images, function(index, image) {
         current_ids.images.push(image.id);
-        if (image.id === target) {
-            type = 'p';
-        }
     });
 
     $.each(current_elements.sounds, function(index, sound) {
         current_ids.sounds.push(sound.id);
-        if (sound.id === target) {
-            type = 's';
-        }
     });
 
     $.ajax({
@@ -41,9 +37,9 @@ click_element = function(target) {
         success: build_gui,
         fail: function() { console.log("error getting moar sounds and images") }
     });
-}
+};
 
-build_gui = function(data) {
+var build_gui = function(data) {
     console.log(data);
     current_elements = data;
     $("#photo_container").empty();
